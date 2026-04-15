1e60f2f2e0e90e2e3ef5ecdfb845dc17fba3390e
Wasim Pakhtoon <myflow.rep@gmail.com>
2026-04-15 15:26:56 +0530

    Update notion backend: added empty body to query

--- api/notion.js
+++ api/notion.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
           "Notion-Version": "2022-06-28",
           "Content-Type": "application/json",
         },
+        body: JSON.stringify({})
       }
     );
 

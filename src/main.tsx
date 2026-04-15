f7cf0acd521f99dded44d9adf7978bf647d9d269
Wasim Pakhtoon <myflow.rep@gmail.com>
2026-04-15 13:20:10 +0530

    added client portal button

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import ClientPortal from './ClientPortal.tsx';
-import ClientPortalShayan from './ClientPortalShayan.tsx';
 import './index.css';
 
-const path = window.location.pathname;
-const isPortalShayan = path.startsWith('/portal/shayan');
-const isPortal = path.startsWith('/portal') && !isPortalShayan;
+const isPortal = window.location.pathname.startsWith('/portal');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isPortalShayan ? <ClientPortalShayan /> : (isPortal ? <ClientPortal /> : <App />)}
+    {isPortal ? <ClientPortal /> : <App />}
   </StrictMode>,
 );
+
 

02b025f84f639aaa57a42115ce25a2e14101f829
wasim pakhtoon <myflow.rep@gmail.com>
2026-04-23 21:23:14 +0530

    added new api supa base

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-// Prevent crashing if env variables are missing
-export const supabase = (supabaseUrl && supabaseKey) 
-  ? createClient(supabaseUrl, supabaseKey) 
-  : null as any
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
